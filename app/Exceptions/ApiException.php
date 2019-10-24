3490a340b004f2d7c3eb5cbd795d3e20dfd6238b
tangwei666 <18670055879@163.com>
2019-10-24 14:27:58 +0800

    。。

--- app/Exceptions/ApiException.php
+++ app/Exceptions/ApiException.php
@@ -1,7 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: lwx
- * Date: 2019/10/24
- * Time: 14:16
- */
+namespace App\Exceptions;
+
+use Illuminate\Http\Response;
+
+class ApiException extends \Exception
+{
+    function __construct($message='',$code = Response::HTTP_BAD_REQUEST)
+    {
+        parent::__construct($message,$code);
+    }
+}
