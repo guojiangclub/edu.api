98f234479565098bf866853f3bc29e6e4259defe
tangwei666 <18670055879@163.com>
2019-10-29 11:11:21 +0800

    修复日志错误

--- modules/Edu.Core/src/helpers.php
+++ modules/Edu.Core/src/helpers.php
@@ -86,3 +86,96 @@ if (!function_exists('build_order_no')) {
         return $prefix.$order_id;
     }
 }
+
+if (!function_exists('date_friendly')) {
+    /**
+     * 时间友好型提示风格化（即微博中的XXX小时前、昨天等等）
+     *
+     * 即微博中的 XXX 小时前、昨天等等, 时间超过 $time_limit 后返回按 out_format 的设定风格化时间戳
+     *
+     * @param  int
+     * @param  int
+     * @param  string
+     * @param  array
+     * @param  int
+     * @return string
+     */
+    function date_friendly($date, $time_limit = 604800, $out_format = 'Y-m-d H:i', $formats = null, $time_now = null)
+    {
+
+        if (!$date) {
+            return false;
+        }
+
+        $timestamp = is_numeric($date) ? $date : strtotime($date);
+
+        if ($formats == null) {
+            $formats = array('YEAR' => '%s 年前'
+            , 'MONTH' => '%s 月前'
+            , 'DAY' => '%s 天前'
+            , 'HOUR' => '%s 小时前'
+            , 'MINUTE' => '%s 分钟前'
+            , 'SECOND' => '%s 秒前');
+        }
+
+        $time_now = $time_now == null ? time() : $time_now;
+        $seconds = $time_now - $timestamp;
+
+        if ($seconds == 0) {
+            $seconds = 1;
+        }
+
+        if (!$time_limit OR $seconds > $time_limit) {
+            return date($out_format, $timestamp);
+        }
+
+        $minutes = floor($seconds / 60);
+        $hours = floor($minutes / 60);
+        $days = floor($hours / 24);
+        $months = floor($days / 30);
+        $years = floor($months / 12);
+
+        if ($years > 0) {
+            $diffFormat = 'YEAR';
+        } else {
+            if ($months > 0) {
+                $diffFormat = 'MONTH';
+            } else {
+                if ($days > 0) {
+                    $diffFormat = 'DAY';
+                } else {
+                    if ($hours > 0) {
+                        $diffFormat = 'HOUR';
+                    } else {
+                        $diffFormat = ($minutes > 0) ? 'MINUTE' : 'SECOND';
+                    }
+                }
+            }
+        }
+
+        $dateDiff = null;
+
+        switch ($diffFormat) {
+            case 'YEAR' :
+                $dateDiff = sprintf($formats[$diffFormat], $years);
+                break;
+            case 'MONTH' :
+                $dateDiff = sprintf($formats[$diffFormat], $months);
+                break;
+            case 'DAY' :
+                $dateDiff = sprintf($formats[$diffFormat], $days);
+                break;
+            case 'HOUR' :
+                $dateDiff = sprintf($formats[$diffFormat], $hours);
+                break;
+            case 'MINUTE' :
+                $dateDiff = sprintf($formats[$diffFormat], $minutes);
+                break;
+            case 'SECOND' :
+                $dateDiff = sprintf($formats[$diffFormat], $seconds);
+                break;
+        }
+
+        return $dateDiff;
+    }
+}
